d92789c7ca09518912a30a5d21b66af89b32c74b
PivetAlencar32 <102256663+PivetAlencar32@users.noreply.github.com>
2022-03-23 22:23:47 -0300

    Update no texto para teste
    Adicionei o texto "Mudanca de texto para texte" para realizar um teste

--- lib/main.dart
+++ lib/main.dart
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'Press to add more one',
+              'Mudanca de texto para texte.',
             ),
             Text(
               '$_counter',
